c994b77c686ac3ceb3da4afb36b7d29a651db72d
safayet <safayet2218@gmail.com>
2018-12-10 22:57:09 +0600

    first commit

---
+++ app/src/main/java/com/example/mdsaf/splash/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.mdsaf.splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        }
+}
